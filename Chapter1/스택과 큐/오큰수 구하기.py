b0b7e66d0cce5e1d3e5ec5fdb32e9651b0e02e16
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-06-24 01:42:40 +0900

    자료구조 스택 공부

---
+++ Chapter1/스택과 큐/오큰수 구하기.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+numbers = list(map(int, input().split()))
+answer = [0] * N
+
+for i in range(N):
+	while stack and numbers[stack[-1]] < numbers[i]:
+		answer[stack.pop()] = numbers[i]
+  stack.append(i)
+
+while stack:
+  answer[stack.pop()] = -1
+  
+result = ''
+for i in range(N):
+  result += str(answer[i]) + ''
+
+print(result)
+  
