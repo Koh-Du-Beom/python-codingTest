d5d1983f0765981ee846a9339c0426d9ca3a4de2
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-07-03 23:40:57 +0900

    이진탐색

---
+++ 탐색/원하는 정수 찾기.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+A.sort()
+M = int(input())
+
+target_list = list(map(int, input().split()))
+
+for i in range(N):
+  find = False
+  target = target_list[i]
+  
+  start = 0
+  end = len(A) - 1
+  while start <= end:
+    mid_index = (start + end) // 2
+    mid_value = A[mid_index]
+    if mid_value > target:
+      end = mid_index - 1
+    elif mid_value < target:
+      start = mid_index + 1
+    else:
+      find = True
+      break
+  
+  if find:
+    print(1)
+  else:
+    print(0)
