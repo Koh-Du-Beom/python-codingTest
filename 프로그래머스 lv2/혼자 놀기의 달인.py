ae9da34cfdbb1a3181f82d467e1b55022a7fdcce
Koh-Du-Beom <beanleaf3260@gmail.com>
2024-10-07 14:31:10 +0900

    프로그래머스 문제풀이

--- 프로그래머스 lv2/혼자 놀기의 달인.py
+++ 프로그래머스 lv2/혼자 놀기의 달인.py
@@ -1,19 +1,26 @@
 def solution(cards):
-    i = 0 #반복을위한 변수
+    visited = [False] * len(cards)  # 상자의 방문 여부를 체크할 리스트
     answer_list = []
-    while(i < len(cards)):
-        temp_list = []
-        while(True):
-            if (i+1) in temp_list:
-                answer_list.append(temp_list)
-                break
-            temp_list.append(cards[i])
-            i = cards[i] - 1
-        i += 1
-        
-    print(answer_list)
     
-    answer = 0
-    return answer
-
-print(solution([8,6,3,7,2,5,1,4]))
+    for i in range(len(cards)):
+        if not visited[i]:  # 이미 탐색한 상자는 건너뜀
+            temp_list = []
+            j = i
+            
+            # 상자를 열어가며 그룹을 형성
+            while not visited[j]:
+                temp_list.append(cards[j])
+                visited[j] = True  # 해당 상자를 열었음을 표시
+                j = cards[j] - 1  # 다음 상자의 인덱스
+                
+            # 그룹의 크기를 저장
+            answer_list.append(len(temp_list))
+    
+    # 그룹 크기를 내림차순으로 정렬
+    answer_list.sort(reverse=True)
+    
+    # 그룹이 2개 이상인 경우 최대 점수 계산
+    if len(answer_list) > 1:
+        return answer_list[0] * answer_list[1]
+    else:
+        return 0  # 그룹이 1개 이하인 경우 0점
