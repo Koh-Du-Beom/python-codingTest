81d1a454366ad776755a328b89f5318984bc2ef5
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-12-04 18:27:33 +0900

    Update boj1182.py

--- 강의/chapter2/브루트포스/boj1182.py
+++ 강의/chapter2/브루트포스/boj1182.py
@@ -1,15 +1,14 @@
 import sys
-from itertools import combinations
 input = sys.stdin.readline
+from itertools import combinations
 
 N, S = map(int, input().split())
 lst = list(map(int, input().split()))
 
-cnt = 0
-
+count = 0 
 for i in range(1, N + 1):
-  for j in combinations(lst, i):
-    if sum(j) == S:
-      cnt += 1
-      
-print(cnt)
+  for comb in combinations(lst, i):
+    if sum(comb) == S:
+      count += 1
+
+print(count)
